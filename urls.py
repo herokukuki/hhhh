3e00f9a7ed4170ad5b3d7728ff81d9e0229c6ea9
Takeshi Kanemoto <tak.kanemoto@gmail.com>
2013-02-01 13:12:09 +0900

    Added torrent actions
    Change-Id: Id56f21144f56a7eb1c5d0f32c04fc16cddb599a6

--- urls.py
+++ urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls.defaults import patterns, url
 
-from torrent.views import TorrentMain, TorrentList
+from torrent.views import TorrentMain, TorrentList, TorrentAction
 
 urlpatterns = patterns(
     'torrent.views',
@@ -11,5 +11,10 @@ urlpatterns = patterns(
     url(r'^torrents$',
         view=TorrentList.as_view(),
         name='torrent_torrent_list',
+    ),
+    url(r'^action(?:/(?P<id>[0-9]+))?/(?P<action>start|stop|add)'
+        '(?:/(?P<hash>[0-9a-f]{40}))?$',
+        view=TorrentAction.as_view(),
+        name='torrent_torrent_action',
     ),
 )
--- views.py
+++ views.py
@@ -1,5 +1,6 @@
 from django.db.models import Q
-from django.views.generic import ListView, TemplateView
+from django.http import Http404, HttpResponse
+from django.views.generic import View, ListView, TemplateView
 
 from torrent.models import Torrent
 
@@ -15,3 +16,22 @@ class TorrentList(ListView):
     def get_queryset(self):
         Torrent.objects.sync()
         return Torrent.objects.all()
+
+
+class TorrentAction(View):
+    def get(self, request, *args, **kwargs):
+        if kwargs['action'] == 'stop' or kwargs['action'] == 'start':
+            try:
+                torrent = Torrent.objects.get(base_id=kwargs['id'])
+            except Torrent.DoesNotExist as e:
+                raise Http404()
+
+        if kwargs['action'] == 'stop':
+            result = Torrent.objects.client.stop_torrent(torrent.base_id)
+        elif kwargs['action'] == 'start':
+            result = Torrent.objects.client.start_torrent(torrent.base_id)
+        elif kwargs['action'] == 'add':
+            text = request.GET['text'] if 'text' in request.GET else 'Magnet'
+            magnet = "magnet:?xt=urn:btih:" + kwargs['hash'] + "&dn=" + text
+            result = Torrent.objects.client.add_torrent(magnet)
+        return HttpResponse(result)
