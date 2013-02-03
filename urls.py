fc9584e4096d08a957c41858285a8b20c3d82900
Takeshi Kanemoto <tak.kanemoto@gmail.com>
2013-02-03 17:42:42 +0900

    Only allow superusers to view the app
    Change-Id: I64a14f0d36b37e11b3270437292f3aff2450333c

--- urls.py
+++ urls.py
@@ -1,20 +1,23 @@
 from django.conf.urls.defaults import patterns, url
+from django.contrib.auth.decorators import login_required
+from django.views.generic import TemplateView
 
-from torrent.views import TorrentMain, TorrentList, TorrentAction
+from torrent.views import TorrentList, TorrentAction
 
 urlpatterns = patterns(
     'torrent.views',
     url(r'^$',
-        view=TorrentMain.as_view(),
+        view=login_required(
+            TemplateView.as_view(template_name="torrent/torrent.html")),
         name='torrent',
     ),
     url(r'^torrents$',
-        view=TorrentList.as_view(),
+        view=login_required(TorrentList.as_view()),
         name='torrent_torrent_list',
     ),
     url(r'^action(?:/(?P<id>[0-9]+))?/(?P<action>start|stop|add)'
         '(?:/(?P<hash>[0-9a-f]{40}))?$',
-        view=TorrentAction.as_view(),
+        view=login_required(TorrentAction.as_view()),
         name='torrent_torrent_action',
     ),
 )
--- views.py
+++ views.py
@@ -1,25 +1,22 @@
 from django.db.models import Q
 from django.http import Http404, HttpResponse
-from django.views.generic import View, ListView, TemplateView
+from django.views.generic import View, ListView
 
 from torrent.models import Torrent
 
 
-class TorrentMain(TemplateView):
-    """
-    Displays torrent app's top page.
-    """
-    template_name="torrent/torrent.html"
-
-
 class TorrentList(ListView):
     def get_queryset(self):
-        Torrent.objects.sync()
-        return Torrent.objects.all()
+        if self.request.user.is_superuser:
+            Torrent.objects.sync()
+            return Torrent.objects.all()
+        return Torrent.objects.none()
 
 
 class TorrentAction(View):
     def get(self, request, *args, **kwargs):
+        if not request.user.is_superuser:
+            return HttpResponse("You don't have the permissions to do that!")
         if kwargs['action'] == 'stop' or kwargs['action'] == 'start':
             try:
                 torrent = Torrent.objects.get(base_id=kwargs['id'])
